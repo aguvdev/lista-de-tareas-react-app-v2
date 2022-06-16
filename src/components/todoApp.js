b172405b2f3dfc01fc93da6a941fb461abed596c
hannagustinvega <hannheitiragustin@hotmail.com>
2022-06-16 13:55:26 -0300

    update: funcionalidad completada

--- src/components/todoApp.js
+++ src/components/todoApp.js
@@ -1,13 +1,10 @@
 import { useState } from "react";
+import Todo from "./todo";
+import '../stylesheets/todoApp.css'
 
 export default function TodoApp() {
-  const [title, setTitle] = useState("Nestor");
+  const [title, setTitle] = useState("");
   const [todos, setTodos] = useState([]);
-
-  function handleClick(e) {
-    e.preventDefault();
-    setTitle("Agustin");
-  }
 
   function handleChange(e) {
     const value = e.target.value;
@@ -27,20 +24,46 @@ export default function TodoApp() {
     const temp = [...todos];
     temp.unshift(newTodo);
 
+    setTodos(temp);
+
+    setTitle("");
+  }
+
+  function handleUpdate(id, value) {
+    const temp = [...todos];
+    const item = temp.find(item => item.id === id);
+    item.title = value;
+    setTodos(temp)
+  }
+
+  function handleDelete(id) {
+    const temp = todos.filter(item => item.id !== id);
     setTodos(temp)
   }
 
   return (
     <div className="todoContainer">
       <form onSubmit={handleSubmit} className="todoCreateForm">
-        <input onChange={handleChange} className="todoInput" />
+        <input onChange={handleChange} className="todoInput" value={title} />
         <input
-          onClick={handleClick}
+          onClick={handleSubmit}
           type="submit"
           value="Create todo"
           className="buttonCreate"
         />
       </form>
+
+      <div className="todosContainer">
+        {
+          todos.map(item => (
+            <Todo
+              key={item.id} 
+              item={item} 
+              onUpdate={handleUpdate}
+              onDelete={handleDelete} /> /* el key es importante para ayudar a react a reconocer el orden en que se van renderizando los elementos y distinguirlos en jsx */
+          ))
+        }
+      </div>
     </div>
   );
 }
